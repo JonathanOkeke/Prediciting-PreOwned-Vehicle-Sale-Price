a9d1e9a4e43cdc59d2d72c7e6891deb4516a3e7d
JonathanOkeke <okekejonathan@hotmail.com>
2021-03-27 16:04:15 +0200

    added v2 of app.py and index.html

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import numpy as np
 import sklearn
 from sklearn.preprocessing import StandardScaler
 app = Flask(__name__)
-model = pickle.load(open('model/xgb_reg_baseline_model.pkl', 'rb'))
+model = pickle.load(open('model/random_forest_reg_baseline_model_1.pkl', 'rb'))
 @app.route('/',methods=['GET'])
 def Home():
     return render_template('index.html')
